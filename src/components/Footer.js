4a56818973e410122e0567b39b8306a3804900ee
Johan Lövberg <Lovberg_93@hotmail.com>
2024-03-28 14:18:45 +0100

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <footer>
+      <menu>
+        <li className="img-footer">
+          <img
+            src={require("../assets/footer-logo.png")}
+            alt="Little Lemon logo"
+          ></img>
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <a
+              href={require("../assets/menu.webp")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Menu
+            </a>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/login">Login</Link>
+          </ul>
+        </li>
+
+        <li>
+          <h1 className="footer-header">Contact</h1>
+          <ul className="footer-links">
+            <li>1234 Pretend road,</li>
+            <li>World</li>
+            <br></br>
+            <li>(123)-456-7890</li>
+            <br></br>
+            <a
+              href="mailto: info@littlelemon.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@littlelemon.com
+            </a>
+          </ul>
+        </li>
+      </menu>
+    </footer>
+  );
+}
